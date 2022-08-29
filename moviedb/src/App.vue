cd085b34bc9c7dc7725b7bfa971f783d62f15628
Hekima <72218314+HekimaGit@users.noreply.github.com>
2022-08-28 22:33:19 -0500

    Add files via upload

--- moviedb/src/App.vue
+++ moviedb/src/App.vue
@@ -1,19 +1,45 @@
+<script setup>
+  function show(num) {
+      if (num == 0) {
+          document.getElementById("Home").style.display = "";
+          document.getElementById("Movies").style.display = "none";
+          document.getElementById("Actors").style.display = "none";
+      } else if (num == 1) {
+          document.getElementById("Movies").style.display = "";
+          document.getElementById("Home").style.display = "none";
+          document.getElementById("Actors").style.display = "none";
+      } else {
+          document.getElementById("Actors").style.display = "";
+          document.getElementById("Home").style.display = "none";
+          document.getElementById("Movies").style.display = "none";
+      }
+  }
+</script>
 
 <template>
-    <div id="TopPane">
-        <h1>Hekima's Movie Database</h1>
-        <div>
-          <button>Home</button>
-          <button>Movies</button>
-          <button>Actors</button>
-        </div>
-    </div>
+  <div id="TopPane">
+      <h1>Hekima's Movie Database</h1>
+      <div>
+        <button @click="show(0)">Home</button>
+        <button @click="show(1)">Movies</button>
+        <button @click="show(2)">Actors</button>
+      </div>
+  </div>
 
-    <div id="Home" style="display: none">
-      <h2>Welcome!</h2>
-    </div>
-    <div id="Movies" style="display: none"></div>
-    <div id="Actors" style="display: none"></div>
+  <div id="Home">
+    <h2>Welcome!</h2>
+  </div>
+
+  <div id="Movies" style="display: none">
+    <h2>Movies</h2>
+    <ul>
+      <b-table items></b-table>
+    </ul>
+  </div>
+
+  <div id="Actors" style="display: none">
+    <h2>Actors</h2>
+  </div>
 </template>
 
 <style>
@@ -48,6 +74,14 @@
 }
 
 #Home {
+  background-color: aliceblue;
+}
+
+#Movies {
+  background-color: aliceblue;
+}
+
+#Actors {
   background-color: aliceblue;
 }
 
